3e80c65093d45a4d557610b3a55b1fea58695ebd
Smiley McSmiles <31331596+Smiley-McSmiles@users.noreply.github.com>
2022-10-04 15:12:20 -0500

    Initial v1.0 Release

---
+++ README.md
@@ -0,0 +1,36 @@
+# INSTALL INSTRUCTIONS
+
+```sh
+git clone https://github.com/Smiley-McSmiles/ovpn-manager
+cd ovpn-manager
+chmod +x setup.sh
+sudo ./setup.sh
+```
+
+# UPDATE INSTRUCTIONS
+`sudo wget -O /bin/ovpn https://raw.githubusercontent.com/Smiley-McSmiles/ovpn-manager/main/ovpn.sh; sudo chmod +x /bin/ovpn`
+
+# USAGE
+
+```sh
+OpenVPN Manager v1.0
+-Created by Smiley McSmiles & XeN
+
+Syntax: ovpn -[COMMAND] [OPTION]
+COMMANDS:
+-s Start OpenVPN
+-S Stop OpenVPN
+-t Status of OpenVPN
+-e Enable OpenVPN
+-d Disable OpenVPN
+-r Restart OpenVPN
+-c Change OpenVPN Connection
+-i Import OpenVPN .ovpn file
+-f Fix OpenVPN permissions
+-v Print OpenVPN Manager's Version
+-b Backup OpenVPN Manager Configurations
+-rb [backup.tar] Restore OpenVPN Manager Configurations
+-h Display this help menu
+```
+
+
---
+++ setup.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+Has_sudo()
+{
+	if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    return 1
+    exit
+  else
+  		return 0
+	fi
+}
+
+Change_variable()
+{
+	# Change_variable varToChange newVarContent VarType sourceFile
+	varToChange=$1
+	newVarContent=$2
+	varType=$3
+	sourceFile=$4
+	if [[ ! -n $varToChange ]] || [[ ! -n $newVarContent ]]; then
+		echo "Function Change_variable requires 2 parameters: varToChange newVarContent"
+		exit
+	elif [[ $varType == "array" ]]; then
+		sed -i -e "s|$varToChange=.*|$varToChange=\($newVarContent\)|g" $sourceFile
+	else
+		sed -i -e "s|$varToChange=.*|$varToChange=$newVarContent|g" $sourceFile
+	fi
+}
+
+Fix_Permissions()
+{
+	Has_sudo
+	chown -Rfv root:openvpn /etc/openvpn
+	chmod -Rfv 660 /etc/openvpn
+	chmod -Rfv ug+X /etc/openvpn
+}
+
+Install_dependancies()
+{
+	Has_sudo
+	packagesNeeded="openvpn"
+	echo "Preparing to install needed dependancies for OpenVPN..."
+
+	if [ -f /etc/os-release ]; then
+		source /etc/os-release
+		crbOrPowertools=
+		os_detected=true
+		echo "ID=$ID"
+		
+			case "$ID" in
+				fedora)				dnf install $packagesNeeded -y ;;
+				rhel)					dnf install $packagesNeeded -y ;;
+				debian)				apt install $packagesNeeded -y ;;
+				ubuntu)				apt install $packagesNeeded -y ;;
+				linuxmint)			apt install $packagesNeeded -y ;;
+				elementary)		apt install $packagesNeeded -y ;;
+				arch)					pacman -Syu $packagesNeeded  ;;
+				endeavouros)		pacman -Syu $packagesNeeded  ;;
+				manjaro)				pacman -Syu $packagesNeeded  ;;
+				opensuse*)			zypper install $packagesNeeded ;;
+			esac
+	else
+		os_detected=false
+		echo "|-------------------------------------------------------------------|"
+		echo "|                       ******WARNING******                         |"
+		echo "|                        ******ERROR******                          |"
+		echo "|               FAILED TO FIND /etc/os-release FILE.                |"
+		echo "|              PLEASE MANUALLY INSTALL THESE PACKAGES:              |"
+		echo "|                             openvpn                               |"
+		echo "|-------------------------------------------------------------------|"
+		
+		read -p "Press ENTER to continue" ENTER
+	fi
+}
+
+Disable_IPv6()
+{
+	if [ -x "$(command -v ufw)" ]; then
+		read -p "Would you like to disable IPv6 in UFW? : [y/N] " disableIPv6
+		if [[ $disableIPv6 == [yY] ]] || [[ $disableIPv6 == [yY][eE][sS] ]]; then
+			Change_variable IPV6 no null /etc/default/ufw
+			ufw disable
+			ufw enable
+		fi
+			ufw reload
+	fi
+}
+
+Setup()
+{
+	Has_sudo
+	Install_dependancies
+	echo "Press ENTER to skip"
+	read -p "Input your VPN Provider : " accountFileName
+	read -p "Input your VPN Account Name : " vpnAccountName
+	read -p "Input your VPN Password : " vpnPassword
+	clear
+	
+	if [[ -n $vpnAccountName ]]; then
+		mkdir -p /etc/openvpn/accounts/
+		mkdir -p /etc/openvpn/client/
+		echo "$accountUserName" >> /etc/openvpn/accounts/$accountFileName
+	 	echo "$accountPassWord" >> /etc/openvpn/accounts/$accountFileName
+		ls -w 1 /etc/openvpn/accounts/
+		chown -Rfv root:openvpn /etc/openvpn/accounts
+ 	fi
+	Fix_Permissions
+	Disable_IPv6
+	mv ovpn.sh /bin/ovpn
+	chmod +x /bin/ovpn
+	ovpn -h -i
+}
+
+Restore()
+{
+	Has_sudo
+	if [ ! -f $1 ]; then
+		echo "ERROR: $1 IS NOT A FILE. TRY AGAIN!"
+		exit
+	fi
+	
+	importTar=$1
+	tar xf $importTar -C /
+	Fix_Permissions
+	Setup
+	ovpn -e -s
+	echo "Complete!"
+	
+}
+
+
+# MAIN
+
+if [[ -n "$1" ]]; then
+	while [[ -n "$1" ]]; do
+		case "$1" in
+			-rb) Restore $2 
+						shift ;;
+			*) echo "Option $1 not recognized" ;;
+		esac
+		shift
+	done
+else
+	Setup
+fi
+
+# END MAIN
